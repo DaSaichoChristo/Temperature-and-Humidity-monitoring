32ef05fdf174815e512556a92cf944c86e9665f3
DaSaichoChristo <chaar0123@outlook.com>
2025-08-04 20:59:29 -0400

    Added connection to Wifi module and .gitignore file

--- embedded/tempsensor.ino
+++ embedded/tempsensor.ino
@@ -1,4 +1,7 @@
 #include "DHT.h"
+#include "ArduinoJson.h"
+#include <WiFi.h>
+#include "secrets.h"
 
 #define DHTPIN 11
 #define DHTTYPE DHT11
@@ -6,10 +9,27 @@
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-  delay(2000);
+  
+  delay(1000);
   Serial.begin(115200);
+  
   dht.begin();
   Serial.println("DHT11 sensor test");
+  
+  delay(1000);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  
+  Serial.println("\nConnecting");
+  
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(100);
+  }
+
+  Serial.println("\nConnected to the WiFi network");
+  Serial.print("Local ESP32 IP: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
